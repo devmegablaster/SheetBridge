4add8506f0fc8dcf4d734f327f9c432d7da1e709
Ojas Tyagi <devmegablaster@gmail.com>
2024-09-30 11:32:16 +0530

    fix: synk auth middleware

--- api/routes/synk.go
+++ api/routes/synk.go
@@ -10,7 +10,7 @@ func registerSynkRoutes(g *echo.Group, cfg *RouterConfig) {
 	synk := g.Group("/synks")
 
 	authMiddleware := middlewares.NewAuth(cfg.DbSvc, cfg.Cfg)
-	g.Use(authMiddleware.Auth)
+	synk.Use(authMiddleware.Auth)
 
 	synkHandler := handlers.NewSynkHandler(cfg.DbSvc, cfg.Cfg)
 
