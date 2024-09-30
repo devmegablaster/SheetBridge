f9670b01dd00e9575b575bb9b5d50d72267cfae2
Ojas Tyagi <devmegablaster@gmail.com>
2024-09-30 14:58:44 +0530

    feat: logger

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -7,11 +7,14 @@ import (
 	"github.com/devmegablaster/SheetBridge/internal/auth"
 	"github.com/devmegablaster/SheetBridge/internal/config"
 	"github.com/devmegablaster/SheetBridge/internal/database"
+	"github.com/devmegablaster/SheetBridge/pkg/logger"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
+	logger.Init()
+
 	cfg := config.NewConfig()
 	cfg.Init()
 
@@ -20,6 +23,7 @@ func main() {
 	auth.InitGoogle(&cfg.Auth)
 
 	e := echo.New()
+	e.HideBanner = true
 	e.Use(middleware.Logger())
 
 	version := e.Group(fmt.Sprintf("/api/%s", cfg.Api.Version))
